a9cbcceb0db841c990583f4556771b8008e77a9d
誠司 <ms01ps02@gmail.com>
2020-06-25 10:40:46 +0900

    scene_manager.hの更新 scene_manager.cppの作成

--- legend/legend/src/scenes/scene_manager.h
+++ legend/legend/src/scenes/scene_manager.h
@@ -9,6 +9,41 @@ namespace scenes {
  */
 class SceneManager {
  public:
+  /**
+   * @brief コンストラクタ
+   */
+  SceneManager();
+  /**
+   * @brief コピーコンストラクタ禁止
+   */
+  SceneManager(const SceneManager &) = delete;
+  /**
+   * @brief コピーコンストラクタ禁止
+   */
+  SceneManager &operator=(const SceneManager &) = delete;
+  /**
+   * @brief デストラクタ
+   */
+  ~SceneManager();
+
+   /**
+   * @brief 更新
+   */
+  void Update();
+  /**
+   * @brief シーンの追加
+   */
+  void Add();
+  /**
+   * @brief シーン遷移
+   */
+  void Change();
+  /**
+   * @brief 描画
+   */
+  void Draw() const;
+
+ private:
 
 };
 
@@ -16,4 +51,3 @@ class SceneManager {
 }  // namespace legend
 
 #endif //! LEGEND_SCENES_SCENE_MANAGER_H_
-#pragma once

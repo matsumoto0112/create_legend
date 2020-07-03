0c11829505a10546aafe0902eb8973e6ed3e0a7d
matsumoto <matsumoto.k017a1377@gmail.com>
2020-07-03 09:46:24 +0900

    escでゲームを落とす

--- legend/legend/src/game/game_device.h
+++ legend/legend/src/game/game_device.h
@@ -4,7 +4,7 @@
 
 /**
  * @file game_device.h
- * @brief ゲーム機器管理
+ * @brief ゲーム機器管理クラス定義
  */
 
 #include "src/directx/directx12_device.h"
@@ -13,15 +13,41 @@
 
 namespace legend {
 namespace game {
+
+/**
+ * @class GameDevice
+ * @brief ゲームにおける機器管理
+ */
 class GameDevice : public util::Singleton<GameDevice> {
  public:
-  GameDevice();
+  /**
+   * @brief デストラクタ
+   */
   ~GameDevice();
-
+  /**
+   * @brief 初期化
+   * @param target_window 現在描画対象となっているウィンドウ
+   * @return 初期化に成功したらtrueを返す
+   */
   bool Init(std::shared_ptr<window::Window> target_window);
+  /**
+   * @brief フレーム更新処理
+   */
   void Update();
-
+  /**
+   * @brief DirectX12デバイスを取得する
+   */
   directx::DirectX12Device& GetDevice() const { return *device_; }
+  /**
+   * @brief 入力デバイスを取得する
+   */
+  input::InputManager& GetInput() const { return *input_manager_; }
+
+ protected:
+  /**
+   * @brief コンストラクタ
+   */
+  GameDevice();
 
  private:
   //! DX12デバイス

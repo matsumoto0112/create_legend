dedb0489315004f50b45e6b6909a2f9ad06cab8c
村山 <miramura0901@gmail.com>
2020-07-03 09:41:03 +0900

    オーディオ関連作成中

---
+++ legend/legend/src/audio/audio_manager.cpp
@@ -0,0 +1,110 @@
+#include "audio_manager.h"
+
+namespace legend {
+namespace audio {
+
+legend::audio::AudioManager::AudioManager() {}
+
+legend::audio::AudioManager::~AudioManager() {
+  // directsound_の解放
+  // if (directsound_) {
+  //  directsound_->Release();
+  //  directsound_ = NULL;
+  //}
+
+  // MasterungVoiceの破棄
+  if (p_xaudio2_mastering_voice_ != nullptr) {
+    p_xaudio2_mastering_voice_->DestroyVoice();
+    p_xaudio2_mastering_voice_ = nullptr;
+  }
+
+  // XAudio2の破棄
+  if (p_xaudio2_ != nullptr) {
+    p_xaudio2_->Release();
+    p_xaudio2_ = nullptr;
+  }
+
+  // COMの終了
+  CoUninitialize();
+}
+
+bool AudioManager::Init(/*HWND* window*/) {
+  // COMの初期化
+  // CoInitialize(NULL);
+  if (FAILED(CoInitializeEx(NULL, COINIT_MULTITHREADED))) {
+    return false;
+  }
+
+  //サウンドデバイス作成
+  // if (DirectSoundCreate8(NULL, &directsound_, NULL) != S_OK) {
+  //  MY_LOG(L"サウンドデバイスの作成に失敗しました\n");
+  //  return false;
+  //}
+  if (FAILED(XAudio2Create(&p_xaudio2_, 0))) {
+    return false;
+  }
+
+  // MasteringVoice作成
+  if (FAILED(p_xaudio2_->CreateMasteringVoice(&p_xaudio2_mastering_voice_))) {
+    return false;
+  }
+
+  //強調レベルを設定
+  // if (FAILED(directsound_->SetCooperativeLevel(*window, DSSCL_NORMAL))) {
+  //  MY_LOG(L"強調レベルの設定に失敗しました\n");
+  //  return false;
+  //}
+
+  return true;
+}
+
+bool AudioManager::LoadWav(std::wstring filename) {
+  //既に読み込み済みかチェック
+  if (base_audiosources_.find(filename) != base_audiosources_.end()) {
+    MY_LOG(L"既に読み込み済み");
+    return false;
+  }
+
+  base_audiosources_[filename] = std::make_unique<AudioSource>();
+
+  // wavの読み込み
+  if (!base_audiosources_[filename]->Init(p_xaudio2_, filename)) {
+    MY_LOG(L"wavの読み込みに失敗しました。\n");
+    base_audiosources_.erase(filename);
+    return false;
+  }
+
+  return true;
+}
+
+bool AudioManager::Play(std::wstring filename) {
+  //指定したファイルが読み込まれているかチェック
+  if (base_audiosources_.find(filename) == base_audiosources_.end()) {
+    MY_LOG(L"読み込まれていないファイルを再生しようとしました。\n");
+    return false;
+  }
+
+  //デバックで鳴らした
+  base_audiosources_[filename]->Play();
+
+  //AudioSource audiosource_;
+  //audiosource_ = std::make_unique<AudioSource>();
+  //audiosource_.Copy(*base_audiosources_[filename]);
+  //audiosource_.Play();
+  //audiosources_.push_back(audiosource_);
+
+  return true;
+}
+
+void AudioManager::Update()
+{
+
+    base_audiosources_[L"../legend/assets/audios/free_3.wav"]->Update();
+
+    for (int i = 0; i < audiosources_.size(); i++) {
+        audiosources_[i]->Update();
+    }
+}
+
+}  // namespace audio
+}  // namespace legend

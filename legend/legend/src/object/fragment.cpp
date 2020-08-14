25e4656568c8cac3b128adb125ac7b0e6d60a18d
matsumoto <matsumoto.k017a1377@gmail.com>
2020-08-14 09:42:11 +0900

    シーン中に消しカスを配置してみるテスト

---
+++ legend/legend/src/object/fragment.cpp
@@ -0,0 +1,33 @@
+#include "src/object/fragment.h"
+
+#include "src/game/game_device.h"
+#include "src/util/resource/resource_names.h"
+
+namespace legend {
+namespace object {
+Fragment::Fragment() : Parent(L"Fragment") {}
+
+Fragment::~Fragment() {}
+
+bool Fragment::Init(const InitializeParameter& params) {
+  if (!Parent::InitBuffer()) {
+    return false;
+  }
+
+  this->transform_ =
+      util::Transform(params.position, params.rotation, params.scale);
+  this->collision_.SetPosition(transform_.GetPosition());
+  this->collision_.SetRotation(transform_.GetRotation());
+  this->collision_.SetScale(transform_.GetScale());
+  this->collision_.SetLength(params.bounding_box_length);
+
+  auto& resource = game::GameDevice::GetInstance()->GetResource();
+  model_ =
+      resource.GetModel().Get(util::resource::resource_names::model::FRAGMENT);
+  return true;
+}
+
+bool Fragment::Update() { return true; }
+
+}  // namespace object
+}  // namespace legend

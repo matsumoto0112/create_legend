d2de0e62ac2777fccc1f7d202264ca5337229079
matsumoto <matsumoto.k017a1377@gmail.com>
2020-08-26 01:47:39 +0900

    デバッグ用設定の解除忘れ

--- legend/legend/src/system/turn_system.cpp
+++ legend/legend/src/system/turn_system.cpp
@@ -185,7 +185,7 @@ bool TurnSystem::Init(const std::string& stage_name) {
           directx::descriptor_heap::heap_parameter::LocalHeapID::ONE_PLAY),
       L"L_CB");
 
-  current_turn_ = 16;
+  current_turn_ = 0;
   return true;
 }
 

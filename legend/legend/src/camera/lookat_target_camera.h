05bf8c9ccbf95f0873234b1568ceb05fc7e96db5
matsumoto <matsumoto.k017a1377@gmail.com>
2020-09-11 10:27:28 +0900

    add comment

--- legend/legend/src/camera/lookat_target_camera.h
+++ legend/legend/src/camera/lookat_target_camera.h
@@ -3,7 +3,7 @@
 
 /**
  * @file lookat_target_camera.h
- * @brief
+ * @brief ターゲットを注視するカメラクラス定義
  */
 
 #include "src/actor/actor.h"
@@ -11,10 +11,25 @@
 
 namespace legend {
 namespace camera {
+
+/**
+ * @brief ターゲットを注視するカメラ
+ */
 class LookatTargetCamera : public PerspectiveCamera {
  public:
   LookatTargetCamera();
   virtual ~LookatTargetCamera();
+  /**
+   * @brief 初期化
+   * @param name カメラ名
+   * @param target 注視する対象
+   * @param position 設置する座標
+   * @param fov FOV
+   * @param ratio アスペクト比
+   * @param up_vector カメラの上方向
+   * @param near_z 最近点
+   * @param far_z 最遠点
+   */
   virtual bool Init(const std::wstring& name, actor::Actor* target,
                     const math::Vector3& position, float fov,
                     float aspect_ratio,
